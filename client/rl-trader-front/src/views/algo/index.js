50846d52de750173d01fdcd0e10cab1eaacd7d0a
roacult <d.b@swissdigilab.com>
2022-06-10 12:24:15 +0100

    fixing import error

--- client/rl-trader-front/src/views/algo/index.js
+++ client/rl-trader-front/src/views/algo/index.js
@@ -1,6 +1,5 @@
-import { CircularProgress } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
-// import AlgoView from './AlgoView';
+import { CircularProgress } from '@material-ui/core';
 import AlgoView from 'src/views/algo/AlgoView';
 
 const AlgoPage = ({
